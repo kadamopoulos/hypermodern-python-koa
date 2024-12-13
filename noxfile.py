5dc84a223581fe9624307fd2b1a9d2e0fd39e0d0
edsml-ka2523 <konstantinos.adamopoulos23@imperial.ac.uk>
2024-12-13 22:47:16 +0000

    added nox and a noxfile

---
+++ noxfile.py
@@ -0,0 +1,7 @@
+import nox
+
+
+@nox.session(python=["3.12"])
+def tests(session):
+    session.run("poetry", "install", external=True)
+    session.run("poetry", "run", "pytest", "--cov", external=True)
