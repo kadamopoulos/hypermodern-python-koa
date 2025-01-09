acbbafdbcd826d8a566e9b8592ba695244e14d61
edsml-ka2523 <konstantinos.adamopoulos23@imperial.ac.uk>
2025-01-09 19:40:53 +0000

    add language functionality to wikipedia

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,5 +1,6 @@
 import click.testing
 import pytest
+import requests
 
 from hypermodern_python_koa import console
 
@@ -7,16 +8,6 @@ from hypermodern_python_koa import console
 @pytest.fixture
 def runner():
     return click.testing.CliRunner()
-
-
-@pytest.fixture
-def mock_requests_get(mocker):
-    mock = mocker.patch("requests.get")
-    mock.return_value.__enter__.return_value.json.return_value = {
-        "title": "Lorem Ipsum",
-        "extract": "Lorem ipsum dolor sit amet",
-    }
-    return mock
 
 
 def test_main_succeeds(runner, mock_requests_get):
@@ -45,3 +36,8 @@ def test_main_fails_on_request_error(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 1
 
+
+def test_main_prints_message_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = requests.RequestException
+    result = runner.invoke(console.main)
+    assert "Error" in result.output
