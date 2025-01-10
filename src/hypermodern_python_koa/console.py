fb662c4752754b73088ff8bbc8731025a257859a
edsml-ka2523 <konstantinos.adamopoulos23@imperial.ac.uk>
2025-01-10 16:04:07 +0000

    add end to end tests

--- src/hypermodern_python_koa/console.py
+++ src/hypermodern_python_koa/console.py
@@ -16,3 +16,24 @@ def main():
 
     click.secho(title, fg="green")
     click.echo(textwrap.fill(extract))
+
+
+@click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
+@click.version_option(version=__version__)
+def main(language):
+    """The hypermodern Python project."""
+    data = wikipedia.random_page(language=language)
+
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
