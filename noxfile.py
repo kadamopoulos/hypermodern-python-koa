c925de339022897723258eed83622fc36ccaf9d4
edsml-ka2523 <konstantinos.adamopoulos23@imperial.ac.uk>
2025-01-12 22:17:53 +0000

    add linting to poetry

--- noxfile.py
+++ noxfile.py
@@ -1,17 +1,30 @@
+import tempfile
+
 import nox
 
-nox.options.sessions = "lint", "tests"
+
+nox.options.sessions = "lint", "safety", "tests"
+locations = "src", "tests", "noxfile.py"
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
 @nox.session(python=["3.12"])
-def tests(session):
-    args = session.posargs or ["--cov"]
-    args = session.posargs or ["--cov", "-m", "not e2e"]
-    session.run("poetry", "install", external=True)
-    session.run("poetry", "run", "pytest", *args, external=True)
-
-
-locations = "src", "tests", "noxfile.py"
+def black(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "black")
+    session.run("black", *args)
 
 
 @nox.session(python=["3.12"])
@@ -28,7 +41,25 @@ def lint(session):
 
 
 @nox.session(python=["3.12"])
-def black(session):
-    args = session.posargs or locations
-    session.install("black")
-    session.run("black", *args)
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        install_with_constraints(
+            session,
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        install_with_constraints(session, "safety")
+        session.run("safety", "scan", f"--file={requirements.name}", "--full-report")
+
+
+@nox.session(python=["3.12"])
+def tests(session):
+    args = session.posargs or ["--cov"]
+    args = session.posargs or ["--cov", "-m", "not e2e"]
+    session.run("poetry", "install", external=True)
+    session.run("poetry", "run", "pytest", *args, external=True)
