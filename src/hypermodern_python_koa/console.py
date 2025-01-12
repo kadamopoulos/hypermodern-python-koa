e2fe27bf8758558ccef2ba66ba0a187378afca39
edsml-ka2523 <konstantinos.adamopoulos23@imperial.ac.uk>
2025-01-12 17:57:39 +0000

    linting

--- src/hypermodern_python_koa/console.py
+++ src/hypermodern_python_koa/console.py
@@ -1,21 +1,7 @@
 import textwrap
 import click
-import requests
 
 from . import __version__, wikipedia
-
-
-@click.command()
-@click.version_option(version=__version__)
-def main():
-    """The hypermodern Python project."""
-    data = wikipedia.random_page()
-
-    title = data["title"]
-    extract = data["extract"]
-
-    click.secho(title, fg="green")
-    click.echo(textwrap.fill(extract))
 
 
 @click.command()
